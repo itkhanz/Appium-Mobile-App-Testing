63c80d3c020e2f49e0593af220d85d6bc5c75615
itkhanz <itkhanz@gmail.com>
2023-06-19 21:05:38 +0200

    android keyboard interactions

--- Appium-OC/src/test/java/com/itkhanz/practice/drivercommands/android/AppLockUnlock.java
+++ Appium-OC/src/test/java/com/itkhanz/practice/drivercommands/android/AppLockUnlock.java
@@ -5,12 +5,16 @@ import com.itkhanz.practice.constants.Platform;
 import com.itkhanz.practice.utils.DriverFactory;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class AppLockUnlock {
@@ -21,16 +25,30 @@ public class AppLockUnlock {
         driver = DriverFactory.initializeDriver(Platform.ANDROID, Apps.APIDEMOS);
     }
 
-    /*@AfterTest(alwaysRun = true)
+    @AfterTest(alwaysRun = true)
     public void tearDown() {
         DriverFactory.shutdownDriver();
-    }*/
+    }
 
     /*
     lockDevice(Duration duration) locks the dveice for duration and then unlocks it
      */
     @Test
     public void test_deviceLockForDuration() throws InterruptedException {
+        /*DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+
+        caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+        String appUrl = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
+                + File.separator + "resources" + File.separator + "apps" + File.separator + "ApiDemos-debug.apk";
+        caps.setCapability(MobileCapabilityType.APP, appUrl);
+
+        URL url = new URL("http://0.0.0.0:4723/");
+
+        driver = new AndroidDriver(url, caps);*/
+
        ((AndroidDriver) driver).lockDevice(Duration.ofMillis(5000));
         Thread.sleep(10000);
 
---
+++ Appium-OC/src/test/java/com/itkhanz/practice/drivercommands/android/InteractWithKeyboard.java
@@ -0,0 +1,114 @@
+package com.itkhanz.practice.drivercommands.android;
+
+import com.google.common.collect.ImmutableMap;
+import com.itkhanz.practice.constants.Apps;
+import com.itkhanz.practice.constants.Platform;
+import com.itkhanz.practice.utils.DriverFactory;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.time.Duration;
+
+/*
+Documentation
+https://www.javadoc.io/doc/io.appium/java-client/6.0.0/io/appium/java_client/android/nativekey/PressesKey.html
+https://developer.android.com/reference/android/view/KeyEvent
+ */
+public class InteractWithKeyboard {
+    private static AppiumDriver driver;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+        driver = DriverFactory.initializeDriver(Platform.ANDROID, Apps.APIDEMOS);
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void tearDown() {
+        DriverFactory.shutdownDriver();
+    }
+
+    /*
+    We can press the keys on android keyboard and also check if the keyboard is displayed or not
+     */
+    @Test
+    public void test_keyboard() throws InterruptedException {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+
+        //Scroll to element using UiScrollable
+        driver.findElement(AppiumBy.androidUIAutomator(
+                        "new UiScrollable(scrollable(true).instance(0))" +
+                                ".scrollIntoView(textContains(\"TextFields\").instance(0))")
+                )
+                .click();
+
+        By editText = AppiumBy.id("io.appium.android.apis:id/edit");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement textField = wait.until(ExpectedConditions.visibilityOfElementLocated(editText));
+
+        textField.click();
+        Assert.assertTrue(((AndroidDriver) driver).isKeyboardShown());
+
+        ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.A));
+        ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.B));
+        ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.C));
+
+        Assert.assertEquals(textField.getText(), "abc");
+
+        ((AndroidDriver) driver).hideKeyboard();
+        Assert.assertFalse(((AndroidDriver) driver).isKeyboardShown());
+
+    }
+
+    /*
+    KeyEvent is not only specific to the keyboard, it is for the device so we can enter the keys such as HOME, BACK
+     */
+    @Test
+    public void test_deviceKeys() throws InterruptedException {
+        By views = AppiumBy.accessibilityId("Views");
+        driver.findElement(views).click();
+
+        By gallery = AppiumBy.accessibilityId("Gallery");
+        WebElement galleryElment = driver.findElement(gallery);
+        galleryElment.click();
+
+        Thread.sleep(2000);
+        ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.BACK));
+        Thread.sleep(2000);
+
+        Assert.assertTrue(galleryElment.isDisplayed());
+
+        ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.HOME));
+        Thread.sleep(2000);
+        var appState = ((AndroidDriver) driver).queryAppState("io.appium.android.apis"); //takes appPackage as parameter
+        System.out.println(appState.toString());
+        Assert.assertEquals(appState.toString(), "RUNNING_IN_BACKGROUND");
+    }
+
+    /*
+    we can also open other apps with keyEvent such as calendar, calculator etc.
+     */
+    @Test
+    public void test_openAppsWithKeyEvent() throws InterruptedException {
+        ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.CALENDAR));
+        Thread.sleep(2000);
+
+        System.out.println(((AndroidDriver) driver).currentActivity());
+        System.out.println(((AndroidDriver) driver).getCurrentPackage());
+
+        Assert.assertEquals(((AndroidDriver) driver).getCurrentPackage(), "com.google.android.calendar");
+
+    }
+}
